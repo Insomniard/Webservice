4be7fde49a32fcb5ddc5650575b8e782450c96f1
jjeong <psmb2006@naver.com>
2021-07-27 18:02:15 +0900

    update

---
+++ src/main/java/com/insomniard/Webservice/Board/dto/updateDto.java
@@ -0,0 +1,19 @@
+package com.insomniard.Webservice.Board.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class updateDto {
+    private String title;
+    private String contents;
+
+    @Builder
+
+    public updateDto(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+}
