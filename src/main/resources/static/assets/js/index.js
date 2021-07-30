99a4cec4d9742c0e76f3e9be6848585eb48c3c26
jjeong <psmb2006@naver.com>
2021-07-30 19:05:11 +0900

    registration error solved

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,23 +1,24 @@
+
 var main = {
     init : function (){
         var _this = this;
         $('#btn-registration').on('click',function (){
             _this.save();
         });
-
     },
+
     save : function (){
         var data = {
          title : $('#title').val(),
          author: $('#author').val(),
-         contents: $('#contents').val()
+         contents: $('#content').val()
         };
 
         $.ajax({
             type: 'POST',
             url: '/api/v1/board',
             dataType: 'json',
-            contentsType:'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function (){
             alert('작성 완료');
@@ -27,4 +28,5 @@ var main = {
         });
     }
 };
+
 main.init();
