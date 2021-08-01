67b776dbef373bc612d6ffd2082b683f79d86525
jjeong <psmb2006@naver.com>
2021-08-01 16:08:02 +0900

    mainpage

--- src/main/java/com/insomniard/Webservice/Board/controller/IndexController.java
+++ src/main/java/com/insomniard/Webservice/Board/controller/IndexController.java
@@ -1,16 +1,26 @@
 package com.insomniard.Webservice.Board.controller;
 
+import com.insomniard.Webservice.Board.Repository.BoardRepository;
+import com.insomniard.Webservice.Board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final BoardRepository boardRepository;
+    private final BoardService boardService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("board",boardService.findAllDesc());
         return "index";
     }
     @GetMapping("/board/registration")
     public String registration(){
         return "boardRegistration";
     }
+
+
 }
