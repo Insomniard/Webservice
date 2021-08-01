f2dd02c782e3e0e018b93a0a133dca20fe5525d8
jjeong <psmb2006@naver.com>
2021-08-01 16:07:13 +0900

    전체 리스트 출력

--- src/main/java/com/insomniard/Webservice/Board/service/BoardService.java
+++ src/main/java/com/insomniard/Webservice/Board/service/BoardService.java
@@ -2,13 +2,17 @@ package com.insomniard.Webservice.Board.service;
 
 import com.insomniard.Webservice.Board.Repository.BoardRepository;
 import com.insomniard.Webservice.Board.dto.ReadDto;
+import com.insomniard.Webservice.Board.dto.ReadListDto;
 import com.insomniard.Webservice.Board.dto.RegistrationDto;
 import com.insomniard.Webservice.Board.dto.updateDto;
 import com.insomniard.Webservice.Board.entity.Board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class BoardService {
@@ -28,5 +32,10 @@ public class BoardService {
     public ReadDto findById (Long boardId){
         Board entity = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. No = " +boardId));
         return new ReadDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<ReadListDto> findAllDesc(){
+        return boardRepository.findAllDesc().stream().map(board -> new ReadListDto(board)).collect(Collectors.toList());
+
     }
 }
