a7615d5bc68d0f1e57e055059fb6caedbc561583
jjeong <psmb2006@naver.com>
2021-08-01 16:06:52 +0900

    전체 리스트 Query

--- src/main/java/com/insomniard/Webservice/Board/Repository/BoardRepository.java
+++ src/main/java/com/insomniard/Webservice/Board/Repository/BoardRepository.java
@@ -2,7 +2,11 @@ package com.insomniard.Webservice.Board.Repository;
 
 import com.insomniard.Webservice.Board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
-
+    @Query("SELECT b FROM Board b ORDER BY b.boardId DESC ")
+    List<Board> findAllDesc();
 }
