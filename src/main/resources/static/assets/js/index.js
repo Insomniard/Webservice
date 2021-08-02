5b7835beab457dd4682c028d933f12006e66c962
jjeong <psmb2006@naver.com>
2021-08-02 17:58:15 +0900

    login front

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -5,6 +5,13 @@ var main = {
         $('#btn-registration').on('click',function (){
             _this.save();
         });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
 
     save : function (){
@@ -22,6 +29,40 @@ var main = {
             data: JSON.stringify(data)
         }).done(function (){
             alert('작성 완료');
+            window.location.href='/';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function (){
+        var data = {
+            title: $('#title').val(),
+            contents: $('#content').val(),
+        };
+        var boardId = $('#boardId').val();
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/board/'+boardId,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (){
+            alert('수정 완료');
+            window.location.href='/';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    },
+    delete: function (){
+        var boardId = $('#boardId').val();
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/board/'+boardId,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+        }).done(function (){
+            alert('삭제 완료');
+
             window.location.href='/';
         }).fail(function (error){
             alert(JSON.stringify(error));
