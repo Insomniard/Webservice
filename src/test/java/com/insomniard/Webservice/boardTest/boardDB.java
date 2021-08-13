c73abf2b352cfc14db412c04d86df528b90bea7f
jjeong <psmb2006@naver.com>
2021-08-13 10:08:04 +0900

    paging test query

--- src/test/java/com/insomniard/Webservice/boardTest/boardDB.java
+++ src/test/java/com/insomniard/Webservice/boardTest/boardDB.java
@@ -13,38 +13,37 @@ import java.util.stream.IntStream;
 
 @SpringBootTest
 public class boardDB {
-//    @Autowired
-//    private BoardRepository boardRepository;
-//
-//    @Test
-//    public void insertTest(){
-//        IntStream.rangeClosed(1, 100).forEach(i->{
-//            Board board = Board.builder()
-//                    .title("Title test"+i)
-//                    .contents("Content test"+i)
-//                    .author("author test"+i)
-//                    .build();
-//            System.out.println(boardRepository.save(board));
-//        });
-//    }
     @Autowired
     private BoardRepository boardRepository;
+
     @Test
-    public void 연결이되었나요(){
-        LocalDateTime now = LocalDateTime.of(2021,07,28,17,39);
-        Board board = Board.builder()
-                .title("a")
-                .contents("b")
-                .author("c")
-                .build();
-        boardRepository.save(board);
-        List<Board> list = boardRepository.findAll();
-        Board boardTime = list.get(0);
-        System.out.println("=============================================\n"+
-                            "registtTime : " +boardTime.getRegistrationTime()+"\tupdateTime : "+boardTime.getUpdateTime()
-                            +"=============================================");
-//        assertThat(boardTime.getRegistrationTime().isAfter(now));
+    public void insertTest(){
+        IntStream.rangeClosed(1, 100).forEach(i->{
+            Board board = Board.builder()
+                    .title("Title test"+i)
+                    .contents("Content test"+i)
+                    .author("author test"+i)
+                    .build();
+            System.out.println(boardRepository.save(board));
+        });
+    }
+
+//    @Test
+//    public void 연결이되었나요(){
+//        LocalDateTime now = LocalDateTime.of(2021,07,28,17,39);
+//        Board board = Board.builder()
+//                .title("a")
+//                .contents("b")
+//                .author("c")
+//                .build();
+//        boardRepository.save(board);
+//        List<Board> list = boardRepository.findAll();
+//        Board boardTime = list.get(0);
+//        System.out.println("=============================================\n"+
+//                            "registtTime : " +boardTime.getRegistrationTime()+"\tupdateTime : "+boardTime.getUpdateTime()
+//                            +"=============================================");
+////        assertThat(boardTime.getRegistrationTime().isAfter(now));
 //        assertThat(boardTime.getUpdateTime().isAfter(now));
 
     }
-}
+
