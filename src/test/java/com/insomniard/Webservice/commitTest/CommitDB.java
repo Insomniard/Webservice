7d992dd57584a26ae0b9c142c3207b3bd79d0b28
jjeong <psmb2006@naver.com>
2021-09-01 18:47:40 +0900

    user test

--- src/test/java/com/insomniard/Webservice/commitTest/CommitDB.java
+++ src/test/java/com/insomniard/Webservice/commitTest/CommitDB.java
@@ -1,5 +1,9 @@
 package com.insomniard.Webservice.commitTest;
 
+import com.insomniard.Webservice.Account.domain.User;
+import com.insomniard.Webservice.Account.domain.UserRepository;
+import com.insomniard.Webservice.Board.Repository.BoardRepository;
+import com.insomniard.Webservice.Board.entity.Board;
 import com.insomniard.Webservice.commit.entity.Commit;
 import com.insomniard.Webservice.commit.repository.CommitRepository;
 import org.junit.jupiter.api.Test;
@@ -14,6 +18,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CommitDB {
     @Autowired
     private CommitRepository commitRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BoardRepository boardRepository;
 
     @Test
     public void 댓글연결이되었나요(){
@@ -31,6 +39,13 @@ public class CommitDB {
         assertThat(Time.getRegistrationTime().isAfter(now));
         assertThat(Time.getUpdateTime().isAfter(now));
 
+    }
+
+    @Test
+    public void 연관관계테스트(){
+        User user = User.builder().email("insomniaard@gamil.com").build();
+        Board board = Board.builder().title("Jeong test1").contents("Jeong test1").author(user).build();
+        boardRepository.save(board);
     }
 }
 
