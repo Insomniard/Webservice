32b505510a9be374174b47d0559460752f1b9d38
jjeong <psmb2006@naver.com>
2021-09-02 18:30:07 +0900

    board , User Mapping

--- src/main/java/com/insomniard/Webservice/Board/entity/Board.java
+++ src/main/java/com/insomniard/Webservice/Board/entity/Board.java
@@ -1,16 +1,21 @@
 package com.insomniard.Webservice.Board.entity;
 
 import com.insomniard.Webservice.Account.domain.User;
+import com.insomniard.Webservice.commit.entity.Commit;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Entity
 @Getter
+@Setter
 public class Board extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,16 +27,22 @@ public class Board extends BaseEntity{
     @Column(columnDefinition = "TEXT",nullable = true)
     private String contents;
 
+    @Column(nullable = false)
     private String author;
 
     @ManyToOne
+    @JoinColumn(name = "id")
     private User user;
 
+    @OneToMany(mappedBy = "board",cascade = CascadeType.ALL)
+    private List<Commit> commits = new ArrayList<>();
+
     @Builder
-    public Board(String title, String contents, String author) {
+    public Board(User user,String title, String contents, String author) {
         this.title = title;
         this.contents = contents;
         this.author = author;
+        this.user = user;
     }
     public void update(String title, String contents){
         this.title = title;
