bdf77db324148708aefcf4483f7b850e5b7a0cad
jjeong <psmb2006@naver.com>
2021-09-02 18:30:21 +0900

    User, Board Mapping

--- src/main/java/com/insomniard/Webservice/Account/domain/User.java
+++ src/main/java/com/insomniard/Webservice/Account/domain/User.java
@@ -1,11 +1,15 @@
 package com.insomniard.Webservice.Account.domain;
 
 import com.insomniard.Webservice.Board.entity.BaseEntity;
+import com.insomniard.Webservice.Board.entity.Board;
+import com.insomniard.Webservice.commit.entity.Commit;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -14,16 +18,20 @@ public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Board> boards = new ArrayList<>();
 
     @Column(nullable = false)
     private String email;
 
     @Column
     private String picture;
+
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
