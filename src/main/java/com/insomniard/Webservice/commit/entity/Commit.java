dc8444fd2f9e101d6692782f79a946c65795cde4
jjeong <psmb2006@naver.com>
2021-09-03 16:28:56 +0900

    Entity Tostring

--- src/main/java/com/insomniard/Webservice/commit/entity/Commit.java
+++ src/main/java/com/insomniard/Webservice/commit/entity/Commit.java
@@ -1,16 +1,19 @@
 package com.insomniard.Webservice.commit.entity;
 
+import com.insomniard.Webservice.Account.domain.User;
 import com.insomniard.Webservice.Board.entity.BaseEntity;
 import com.insomniard.Webservice.Board.entity.Board;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @NoArgsConstructor
 @Entity
 @Getter
+@ToString
 public class Commit extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,9 +29,10 @@ public class Commit extends BaseEntity {
     private Board board;
 
     @Builder
-    public Commit(String title, String contents, String author) {
+    public Commit(Board board, String title, String contents, String author) {
         this.contents = contents;
         this.author = author;
+        this.board = board;
     }
     public void update(String title, String contents){
         this.contents = contents;
