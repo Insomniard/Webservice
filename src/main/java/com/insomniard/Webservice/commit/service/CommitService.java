cd1f8a9cb20b0d21a4a05a796fa8b6f140b75299
jjeong <psmb2006@naver.com>
2021-09-18 18:08:20 +0900

    commit and board

--- src/main/java/com/insomniard/Webservice/commit/service/CommitService.java
+++ src/main/java/com/insomniard/Webservice/commit/service/CommitService.java
@@ -4,6 +4,7 @@ import com.insomniard.Webservice.Account.domain.User;
 import com.insomniard.Webservice.Account.domain.UserRepository;
 import com.insomniard.Webservice.Board.Repository.BoardRepository;
 import com.insomniard.Webservice.Board.dto.ReadDto;
+import com.insomniard.Webservice.Board.dto.ReadListDto;
 import com.insomniard.Webservice.Board.dto.RegistrationDto;
 import com.insomniard.Webservice.Board.entity.Board;
 import com.insomniard.Webservice.commit.dto.CommitReadDto;
@@ -11,9 +12,13 @@ import com.insomniard.Webservice.commit.dto.CommitRegistrationDto;
 import com.insomniard.Webservice.commit.entity.Commit;
 import com.insomniard.Webservice.commit.repository.CommitRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,10 +31,9 @@ public class CommitService {
     public Long registration(CommitRegistrationDto commitRegistrationDto){
         return commitRepository.save(commitRegistrationDto.toEntity()).getCommitId();
     }
-    @Transactional
-    public CommitReadDto findByBoardId (Long commitId){
-        Commit entity = commitRepository.findById(commitId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. No = " +commitId));
-        return new CommitReadDto(entity);
-    }
+    @Transactional(readOnly = true)
+    public List<CommitReadDto> findAllDesc(Long boardId){
+        return boardRepository.findById(boardId).get().getCommit().stream().map(board -> new CommitReadDto(board)).collect(Collectors.toList());
 
+    }
 }
