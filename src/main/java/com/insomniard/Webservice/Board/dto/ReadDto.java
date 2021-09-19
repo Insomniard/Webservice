569ac6b3282eba7a903b7d75f20c086031de802d
jjeong <psmb2006@naver.com>
2021-09-19 23:55:29 +0900

    commit dto

--- src/main/java/com/insomniard/Webservice/Board/dto/ReadDto.java
+++ src/main/java/com/insomniard/Webservice/Board/dto/ReadDto.java
@@ -1,9 +1,11 @@
 package com.insomniard.Webservice.Board.dto;
 
 import com.insomniard.Webservice.Board.entity.Board;
+import com.insomniard.Webservice.commit.dto.CommitReadDto;
 import com.insomniard.Webservice.commit.entity.Commit;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -12,11 +14,13 @@ public class ReadDto {
     private String title;
     private String contents;
     private String author;
+    private List<CommitReadDto> commits;
 
-    public ReadDto(Board entity) {
+    public ReadDto(Board entity, List<CommitReadDto> commits) {
         this.boardId = entity.getBoardId();
         this.title = entity.getTitle();
         this.contents = entity.getContents();
         this.author = entity.getAuthor();
+        this.commits = commits;
     }
 }
